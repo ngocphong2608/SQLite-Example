e1e70cf622ba1d450e1eeb1c42ea3956e0509e00
ngocphong2608 <ttphong2608@gmail.com>
2016-12-09 20:28:06 +0700

    Added RunManager

--- app/src/main/java/com/example/ngocphong/sqliteexample/fragment/RunFragment.java
+++ app/src/main/java/com/example/ngocphong/sqliteexample/fragment/RunFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ngocphong.sqliteexample.R;
 
@@ -16,16 +18,26 @@ import com.example.ngocphong.sqliteexample.R;
 public class RunFragment extends Fragment {
 
 
-    public RunFragment() {
-        // Required empty public constructor
+    private Button mStartButton, mStopButton;
+    private TextView mStartedTextView, mLatitudeTextView,
+            mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_run, container, false);
+        View view = inflater.inflate(R.layout.fragment_run, container, false);
+        mStartedTextView = (TextView)view.findViewById(R.id.run_startedTextView);
+        mLatitudeTextView = (TextView)view.findViewById(R.id.run_latitudeTextView);
+        mLongitudeTextView = (TextView)view.findViewById(R.id.run_longitudeTextView);
+        mAltitudeTextView = (TextView)view.findViewById(R.id.run_altitudeTextView);
+        mDurationTextView = (TextView)view.findViewById(R.id.run_durationTextView);
+        mStartButton = (Button)view.findViewById(R.id.run_startButton);
+        mStopButton = (Button)view.findViewById(R.id.run_stopButton);
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/ngocphong/sqliteexample/location/RunManager.java
@@ -0,0 +1,73 @@
+package com.example.ngocphong.sqliteexample.location;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+/**
+ * Created by ngocphong on 09/12/2016.
+ */
+public class RunManager {
+    private static final String TAG = "RunManager";
+    public static final String ACTION_LOCATION =
+            "com.bignerdranch.android.runtracker.ACTION_LOCATION";
+    private static RunManager sRunManager;
+    private Context mAppContext;
+    private LocationManager mLocationManager;
+
+    // The private constructor forces users to use RunManager.get(Context)
+    private RunManager(Context appContext) {
+        mAppContext = appContext;
+        mLocationManager = (LocationManager) mAppContext
+                .getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public static RunManager get(Context c) {
+        if (sRunManager == null) {
+            // Use the application context to avoid leaking activities
+            sRunManager = new RunManager(c.getApplicationContext());
+        }
+        return sRunManager;
+    }
+
+    private PendingIntent getLocationPendingIntent(boolean shouldCreate) {
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        int flags = shouldCreate ? 0 : PendingIntent.FLAG_NO_CREATE;
+        return PendingIntent.getBroadcast(mAppContext, 0, broadcast, flags);
+    }
+
+    public void startLocationUpdates() {
+        String provider = LocationManager.GPS_PROVIDER;
+        // Start updates from the location manager
+        PendingIntent pi = getLocationPendingIntent(true);
+        if (ActivityCompat.checkSelfPermission(mAppContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mAppContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(mAppContext, "Location Permission is denied", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mLocationManager.requestLocationUpdates(provider, 0, 0, pi);
+    }
+    public void stopLocationUpdates() {
+        PendingIntent pi = getLocationPendingIntent(false);
+        if (pi != null) {
+            mLocationManager.removeUpdates(pi);
+            pi.cancel();
+        }
+    }
+    public boolean isTrackingRun() {
+        return getLocationPendingIntent(false) != null;
+    }
+}
