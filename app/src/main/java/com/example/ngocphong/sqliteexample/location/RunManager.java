e779e6a381663c476bfc50b384c8fc9e22ced0c9
ngocphong2608 <ttphong2608@gmail.com>
2016-12-11 21:28:51 +0700

    Updated Get Last Known Location

--- app/src/main/java/com/example/ngocphong/sqliteexample/location/RunManager.java
+++ app/src/main/java/com/example/ngocphong/sqliteexample/location/RunManager.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
@@ -43,6 +44,15 @@ public class RunManager {
 
     public void startLocationUpdates() {
         String provider = LocationManager.GPS_PROVIDER;
+
+        // Get the last known location and broadcast it if you have one
+        Location lastKnown = mLocationManager.getLastKnownLocation(provider);
+        if (lastKnown != null) {
+            // Reset the time to now
+            lastKnown.setTime(System.currentTimeMillis());
+            broadcastLocation(lastKnown);
+        }
+
         // Start updates from the location manager
         PendingIntent pi = getLocationPendingIntent(true);
         if (ActivityCompat.checkSelfPermission(mAppContext, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -60,6 +70,13 @@ public class RunManager {
         }
         mLocationManager.requestLocationUpdates(provider, 0, 0, pi);
     }
+
+    private void broadcastLocation(Location location) {
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        broadcast.putExtra(LocationManager.KEY_LOCATION_CHANGED, location);
+        mAppContext.sendBroadcast(broadcast);
+    }
+
     public void stopLocationUpdates() {
         PendingIntent pi = getLocationPendingIntent(false);
         if (pi != null) {
